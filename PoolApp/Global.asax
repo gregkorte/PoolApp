3e6b5408ae59bfa54c2e7bea9f0a50462f42717c
gregkorte <gryphionsroost@yahoo.com>
2015-03-18 14:00:59 -0500

    Initial MVC setup

---
+++ PoolApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PoolApp.MvcApplication" Language="C#" %>
---
+++ PoolApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PoolApp.Startup))]
+namespace PoolApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
