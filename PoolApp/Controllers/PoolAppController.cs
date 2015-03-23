3b0b65bb069f3758b9158a8ab8a12cd35148c1d8
gregkorte <gryphionsroost@yahoo.com>
2015-03-23 15:20:53 -0500

    Api requests for products working.

--- PoolApp/Controllers/PoolAppController.cs
+++ PoolApp/Controllers/PoolAppController.cs
@@ -13,6 +13,8 @@ namespace PoolApp.Controllers
     public class PoolAppController : ApiController
     {
         private CustomerRepo customer_repo = new CustomerRepo();
+        private ProductRepo product_repo = new ProductRepo();
+        private ServiceRepo service_repo = new ServiceRepo();
 
         //------------//
         //  CUSTOMER  //
@@ -67,6 +69,120 @@ namespace PoolApp.Controllers
             json.Data = new
             {
                 customer
+            };
+            return json;
+        }
+
+        //------------//
+        //  PRODUCTS  //
+        //------------//
+        //GET: /api/PoolApp/Products/userID
+        [HttpGet]
+        [Route("Products/{userID}")]
+        public System.Web.Mvc.JsonResult GetProducts(string userID)
+        {
+            //return product_repo.GetAllByUserId(UserID);
+            var products = product_repo.GetAllByUserId(userID);
+            var json = new System.Web.Mvc.JsonResult();
+            json.Data = new
+            {
+                products
+            };
+            return json;
+        }
+
+        [HttpPost]
+        [Route("Products/{userID}")]
+        public HttpResponseMessage Products(string userID, [FromBody] Product product)
+        {
+            product_repo.Add(product);
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        [HttpPut]
+        [Route("Products/{userID}/Edit/{id}")]
+        public HttpResponseMessage EditProduct([FromBody] Product product)
+        {
+            product_repo.Edit(product);
+            return new HttpResponseMessage(HttpStatusCode.OK)
+;
+        }
+
+        [HttpDelete]
+        [Route("Products/{userID}/Delete/{id}")]
+        public HttpResponseMessage DeleteProduct(int id)
+        {
+            product_repo.Delete(id);
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        //GET: /api/PoolApp/Products/userID/id
+        [HttpGet]
+        [Route("Products/{userID}/{id}")]
+        public System.Web.Mvc.JsonResult GetProducts(int id)
+        {
+            var product = product_repo.GetById(id);
+            var json = new System.Web.Mvc.JsonResult();
+            json.Data = new
+            {
+                product
+            };
+            return json;
+        }
+
+        //------------//
+        //  SERVICES  //
+        //------------//
+        //GET: /api/PoolApp/Services/userID
+        [HttpGet]
+        [Route("Services/{userID}")]
+        public System.Web.Mvc.JsonResult GetServices(string userID)
+        {
+            //return service_repo.GetAllByUserId(UserID);
+            var services = service_repo.GetAllByUserId(userID);
+            var json = new System.Web.Mvc.JsonResult();
+            json.Data = new
+            {
+                services
+            };
+            return json;
+        }
+
+        [HttpPost]
+        [Route("Services/{userID}")]
+        public HttpResponseMessage Services(string userID, [FromBody] Service service)
+        {
+            service_repo.Add(service);
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        [HttpPut]
+        [Route("Services/{userID}/Edit/{id}")]
+        public HttpResponseMessage EditService([FromBody] Service service)
+        {
+            service_repo.Edit(service);
+            return new HttpResponseMessage(HttpStatusCode.OK)
+;
+        }
+
+        [HttpDelete]
+        [Route("Services/{userID}/Delete/{id}")]
+        public HttpResponseMessage DeleteService(int id)
+        {
+            service_repo.Delete(id);
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        //GET: /api/PoolApp/Services/userID/id
+        [HttpGet]
+        [Route("Services/{userID}/{id}")]
+        public System.Web.Mvc.JsonResult GetServices(int id)
+        {
+            var service = service_repo.GetById(id);
+            var json = new System.Web.Mvc.JsonResult();
+            json.Data = new
+            {
+                service
             };
             return json;
         }
